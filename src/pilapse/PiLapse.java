df9c02bdaebd0e47dd60fc95eb3528159a8f3773
Nicklas Boserup <nickeopti@gmail.com>
2017-08-20 15:20:53 +0200

    For testing autorun on the PI

--- src/pilapse/PiLapse.java
+++ src/pilapse/PiLapse.java
@@ -5,8 +5,10 @@ package pilapse;
  */
 public class PiLapse {
     
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Hello, World!\nWritten from my Java application...");
+        Thread.sleep(20*1000);
+        System.out.println("Waiting finished!");
     }
     
 }
